26ad4b7ed60a0436050711820b8033c1e9060b3b
HuangZhuoRui <huangzhuoruihzr@gmail.com>
2026-06-27 21:22:07 +0800

    fix: prevent ad-hoc macOS release from debug entitlement

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -63,8 +63,8 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         //noinspection OldTargetApi
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 103
-        versionName = "1.0.3"
+        versionCode = 104
+        versionName = "1.0.4"
         buildConfigField("String", "GITHUB_REPOSITORY", "\"${releaseRepository.get()}\"")
     }
     packaging {
