29665ef08f8cb026d15c001485e9ead0ee349551
HuangZhuoRui <huangzhuoruihzr@gmail.com>
2026-06-27 21:01:56 +0800

    ci: package macOS release as dmg

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -63,8 +63,8 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         //noinspection OldTargetApi
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 102
-        versionName = "1.0.2"
+        versionCode = 103
+        versionName = "1.0.3"
         buildConfigField("String", "GITHUB_REPOSITORY", "\"${releaseRepository.get()}\"")
     }
     packaging {
